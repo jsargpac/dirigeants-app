105fffd160d26726ab0018aa8fbc47bd051a69cf
Julien <juliencapgras@gmail.com>
2017-09-24 16:33:05 +0200

    request all prices from acquisition

--- controllers/importPrices.js
+++ controllers/importPrices.js
@@ -4,6 +4,7 @@ var querystring = require('querystring');
 var mongoose = require('mongoose');
 var fs = require('fs');
 var util = require('util');
+var async = require('async');
 
 const transaction = require('../models/Transaction.js');
 const stock = require('../models/Stock.js');
@@ -14,25 +15,41 @@ const stock = require('../models/Stock.js');
  */
 exports.importPrices = (req, res, next) => {
 
-    var http = new XMLHttpRequest();
-
-    getCodeFromISIN('FR0004040608', function (err, code) {
-        var startDate = "2017-01-01";
-        var endDate = "2017-09-22";
-        requestQuandlData(code, startDate, endDate);
+    transaction.find({ 'nature': 'Acquisition' }, function (err, transactions) {
+        if (err) return handleError(err);
+        if (transactions != null) {
+            // getData(transactions[0].isin);
+            async.each(transactions, function(transactionFound, callback) {
+                getData(transactionFound.isin);
+            })
+            // transactions.forEach(function(transactionFound) {
+            //     async.parallel(getData(transactionFound.isin));
+            // }, this);
+        }
     });
 
-    function getCodeFromISIN(isinSource, callback) {    
-        stock.findOne({ 'isin': isinSource }, 'code', function (err, stockFound) {
+    function getData(isin) {
+        getCodeFromISIN(isin, function (err, stock) {
+            var startDate = "2017-01-01";
+            var endDate = "2017-09-22";
+            requestQuandlData(stock, startDate, endDate);
+        });
+    }
+
+    function getCodeFromISIN(isinSource, callback) {
+        stock.findOne({ 'isin': isinSource }, function (err, stockFound) {
             if (err) return handleError(err);
-            console.log(stockFound.code);
-            callback(null, stockFound.code);
+            if (stockFound != null) {
+                console.log(stockFound.code);
+                callback(null, stockFound);
+            }
         })
     }
 
     //AlphaVantage API Key: 974UTD95QA2DV3Y5
     function requestAlphaVantageData(symbol) {
-        var quandl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=974UTD95QA2DV3Y5";
+        var alpha = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=974UTD95QA2DV3Y5";
+        var http = new XMLHttpRequest();
         http.open("GET", quandl, true);
         http.onreadystatechange = function () {
             if (http.readyState == 4 && http.status == 200) {
@@ -44,36 +61,38 @@ exports.importPrices = (req, res, next) => {
     }
 
     // Quandl API Key: x-sv5jiML9zikPj8wjJy
-    function requestQuandlData(symbol, startDate, endDate) {
-        var quandl = "https://www.quandl.com/api/v3/datasets/EURONEXT/" + symbol + "?start_date=" + startDate +
+    function requestQuandlData(stock, startDate, endDate) {
+        var quandl = "https://www.quandl.com/api/v3/datasets/EURONEXT/" + stock.code + "?start_date=" + startDate +
             "&end_date=" + endDate + "&api_key=x-sv5jiML9zikPj8wjJy";
-        console.log(quandl);
-        http.open("GET", quandl, true);
-        http.onreadystatechange = function () {
-            if (http.readyState == 4 && http.status == 200) {
-                var response = JSON.parse(http.responseText);
-                console.log(response.dataset.data[0][4]);
-                var dates = response.dataset.data.map(function(value,index) { return value[0]; })
-                dates.reverse();
-                var closes = response.dataset.data.map(function(value,index) { return value[4]; })
-                closes.reverse();
-                updateStockClose(symbol, dates, closes);
+        // console.log(quandl);
+        if (stock != null) {
+            if (!stock.already_imported) {
+                var http = new XMLHttpRequest();
+                http.open("GET", quandl, true);
+                http.onreadystatechange = function () {
+                    if (http.readyState == 4 && http.status == 200) {
+                        var response = JSON.parse(http.responseText);
+                        var dates = response.dataset.data.map(function (value, index) { return value[0]; })
+                        dates.reverse();
+                        var closes = response.dataset.data.map(function (value, index) { return value[4]; })
+                        closes.reverse();
+                        updateStockClose(stock, dates, closes);
+                    }
+                }
+                http.send();
             }
         }
-        http.send();
     }
 
-    function updateStockClose(codeSource, dates, closes) {
-        stock.findOne({ 'code': codeSource }, function (err, stockFound) {
-            if (err) return handleError(err);
-            if (stockFound != null) {
-                console.log("Ajout close : " + stockFound.code + " - " + stockFound.close.length + " / " + dates.length);
-                // if (stockFound.close.length == 0) stockFound.close = [[]];
-                stockFound.close = [[]];
-                for (i=0; i<dates.length; i++) stockFound.close.push([dates[i], closes[i]]);
-                stockFound.save();
-            }
-        })
+    function updateStockClose(stock, dates, closes) {
+        if (stock != null) {
+            console.log("Ajout close : " + stock.code + " - " + stock.close.length + " / " + dates.length);
+            // if (stock.close.length == 0) stock.close = [[]];
+            stock.close = [[]];
+            for (i = 0; i < dates.length; i++) stock.close.push([dates[i], closes[i]]);
+            stock.already_imported = true;
+            stock.save();
+        }
     }
 
 };
--- controllers/testData.js
+++ controllers/testData.js
@@ -14,66 +14,10 @@ const stock = require('../models/Stock.js');
  */
 exports.simpleTest = (req, res, next) => {
 
-    var http = new XMLHttpRequest();
+    var codeSource = "ABCA";
 
-    getCodeFromISIN('FR0004040608', function (err, code) {
-        var startDate = "2017-01-01";
-        var endDate = "2017-09-22";
-        requestQuandlData(code, startDate, endDate);
+    transaction.find({ 'code': codeSource, 'nature': 'Acquisition' }, function (err, transactions) {
+
     });
-
-    function getCodeFromISIN(isinSource, callback) {    
-        stock.findOne({ 'isin': isinSource }, 'code', function (err, stockFound) {
-            if (err) return handleError(err);
-            console.log(stockFound.code);
-            callback(null, stockFound.code);
-        })
-    }
-
-    //AlphaVantage API Key: 974UTD95QA2DV3Y5
-    function requestAlphaVantageData(symbol) {
-        var quandl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=974UTD95QA2DV3Y5";
-        http.open("GET", quandl, true);
-        http.onreadystatechange = function () {
-            if (http.readyState == 4 && http.status == 200) {
-                var response = JSON.parse(http.responseText);
-                console.log(response);
-            }
-        }
-        http.send();
-    }
-
-    // Quandl API Key: x-sv5jiML9zikPj8wjJy
-    function requestQuandlData(symbol, startDate, endDate) {
-        var quandl = "https://www.quandl.com/api/v3/datasets/EURONEXT/" + symbol + "?start_date=" + startDate +
-            "&end_date=" + endDate + "&api_key=x-sv5jiML9zikPj8wjJy";
-        console.log(quandl);
-        http.open("GET", quandl, true);
-        http.onreadystatechange = function () {
-            if (http.readyState == 4 && http.status == 200) {
-                var response = JSON.parse(http.responseText);
-                console.log(response.dataset.data[0][4]);
-                var dates = response.dataset.data.map(function(value,index) { return value[0]; })
-                dates.reverse();
-                var closes = response.dataset.data.map(function(value,index) { return value[4]; })
-                closes.reverse();
-                updateStockClose(symbol, dates, closes);
-            }
-        }
-        http.send();
-    }
-
-    function updateStockClose(codeSource, dates, closes) {
-        stock.findOne({ 'code': codeSource }, function (err, stockFound) {
-            if (err) return handleError(err);
-            if (stockFound != null) {
-                console.log("Ajout close : " + stockFound.code + " - " + stockFound.close.length + " / " + dates.length);
-                // if (stockFound.close.length == 0) stockFound.close = [[]];
-                stockFound.close = [[]];
-                for (i=0; i<dates.length; i++) stockFound.close.push([dates[i], closes[i]]);
-                stockFound.save();
-            }
-        })
-    }
-
+    
 };
